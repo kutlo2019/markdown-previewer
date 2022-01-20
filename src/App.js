13ef2defa7bfe97f1847fe8b2cba46cbd10a328f
kutlo2019 <kutlopopo@rocketmail.com>
2022-01-21 00:50:19 +0200

    install markdown parser and prepare boilerplate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { marked } from 'marked';
 
 function App() {
+
+  const [input, setInput] = useState('')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <textarea id="editor" value={input} onChange={(e) => setInput(e.target.value)}></textarea>
+      <div id="preview">
+        {marked.parse(input)}  
+      </div>      
     </div>
   );
 }
